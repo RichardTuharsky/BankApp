5d9cfe9a661e34c7661bfbc0024d44576c21d83e
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-10-11 19:55:05 +0200

    Developint balance function

--- src/Balance.java
+++ src/Balance.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Balance {
 
+    Scanner sc = new Scanner(System.in);
+
     private int balance_id;
-    private String balance_amount;
+    private int balance_amount;
     private int balance_customer_id;
     private String balance_description;
 
     void addBalance() {
+        long amount = 0;
+        System.out.println("Enter amount you want to add: ");
+        amount = sc.nextLong();
+        balance_amount += amount;
+        System.out.println("Current balance after deposit: " +balance_amount);
 
     }
 
